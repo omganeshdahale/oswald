b15b4de56e1a3fe5df21f1bff2afd7ddec13bc1e
Om Dahale <om.g.dahale@gmail.com>
2021-03-06 18:05:10 +0530

    remove latency detail in ping command

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: "ping",
 	description: "Shows Latency.",
 	execute(message, args, client) {
-		message.channel.send(`Latency is \`${Date.now() - message.createdTimestamp}ms\`.\nAPI Ping is \`${client.ws.ping}ms\`.`);
+		message.channel.send(`API Ping is \`${client.ws.ping}ms\`.`);
 	}
 }
