ac45390993de3431cbf8a93722ca676d1c7fb3c7
Om Dahale <om.g.dahale@gmail.com>
2021-03-07 11:51:01 +0530

    remove messages fetching in clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -9,14 +9,11 @@ module.exports = {
 			return message.channel.send(":no_entry_sign: Please Provide a Valid argument:\n`+clear <number x | 0 < x < 100>`.");
 		}
 
-		message.channel.messages.fetch({limit:parseInt(args[0], 10)+1})
-		.then(messages => {
-			message.channel.bulkDelete(messages)
-			.then(collection => {
-				message.channel.send(`:white_check_mark: Deleted \`${collection.size}\` Message(s).`)
-				.then(message => {
-					message.delete({timeout: 3000})
-				}).catch(console.error);
+		message.channel.bulkDelete(parseInt(args[0], 10)+1, true)
+		.then(collection => {
+			message.channel.send(`:white_check_mark: Deleted \`${collection.size}\` Message(s).`)
+			.then(message => {
+				message.delete({timeout: 3000})
 			}).catch(console.error);
 		}).catch(console.error);
 	}
